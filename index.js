db92339515714f8aa160ac8986da2f63b1113113
Daniel A Koontz <dak.7997@gmail.com>
2020-06-14 03:16:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyPrependKitten(name) {
   return kittens
 }
 
-function destructivelyRemoveLastKitten(name) {
+function destructivelyRemoveLastKitten() {
   kittens.pop()
   return kittens
 }
